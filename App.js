84508b3710d7c171ebd57d24e0ca0d6e2df29baa
vishal <vishal13pandir@gmail.com>
2024-08-24 17:46:59 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  // Function to handle button press
+  const handlePress = () => {
+    console.log('Button clicked');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click Me" onPress={handlePress} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
